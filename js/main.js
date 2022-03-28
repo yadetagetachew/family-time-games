38c071d4b9881a1dec614ae456810f3d0a61d121
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2022-03-28 16:58:53 -0700

    feat:(shopping cart finished)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
-let grandTotal = Number(document.getElementById("total"));
-let shopList;
-let shopItems = document.querySelectorAll(".card");
+// let grandTotal = Number(document.getElementById("total"));
+// let shopList;
+// let shopItems = document.querySelectorAll(".card");
 
 //  get the item Name and Value of the item clicked
 
 function addItem(gValue){
-    
+
     console.log(gValue);
-    let result = Number(grandTotal);
-    let gVar = Number(gValue);
-    console.log(typeof gVar)
-    grandTotal = result + gVar;
-    console.log(typeof grandTotal);
-    console.log(grandTotal);    
 
+    let grandTotal = document.getElementById("total");
+
+    console.log(grandTotal.innerText);
+
+    total.innerText = Number(grandTotal.innerText) + Number(gValue);
+
+    console.log(total);    
 }
-
-// shopItems.forEach(function (value) {
-//     console.log(value);
-//     value.addEventListener('click', function (e) {
-//         grandTotal.value += e.value;
-//         console.log("grandTotal.value");
-//     })
-// })
 /*  put the clicked item into the shopping list 
     add to an array*/
 
